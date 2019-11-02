88b9c073af7e475256cd98ab2c0735368e73435e
evanyeh <eyeh2809@yahoo.com>
2019-11-01 19:43:43 -0700

    Add helpers for image capture

--- app/camera.py
+++ app/camera.py
@@ -3,8 +3,13 @@ from datetime import datetime
 from sh import gphoto2 as gp
 import signal, os, subprocess
 
+clearCommand = ["--folder", "/store_00010001/DCIM/100D5300", "-R", "--delete-all-files"]
+triggerCommand = ["--trigger-capture"]
+downloadCommand = ["--get-all-files"]
+captureAndDownloadCommand = ["--capture-image-and-download", "--filename", "name.JPG"]
+save_location = ""
 # Kill the gphoto2 process that starts whenever we connect the camera.
-def killgphoto2process():
+def kill_gphoto2_process():
     p = subprocess.Popen(['ps', '-A'], stdout=subprocess.PIPE)
     out, err = p.communicate()
 
@@ -15,41 +20,39 @@ def killgphoto2process():
             pid = int(line.split(None,1)[0])
             os.kill(pid, signal.SIGKILL)
 
+def init(album_location):
+    kill_gphoto2_process()
+    create_save_folder(album_location)
 
-shot_date = datetime.now().strftime("%Y.%m.%d")
-shot_time = datetime.now().strftime("%Y.%m.%d %H:%M:%S")
-picID = "testpics"
-
-clearCommand = ["--folder", "/store_00010001/DCIM/100D5300", "-R", "--delete-all-files"]
-triggerCommand = ["--trigger-capture"]
-downloadCommand = ["--get-all-files"]
-
-folder_name = picID + shot_date
-save_location = "/home/pi/Desktop/gphoto2/images/" + folder_name 
-
-def createSaveFolder():
+def create_save_folder(album_location):
     try:
-        os.makedirs(save_location)
+        os.makedirs(album_location)
     except:
         print("failed to create the new directory")
-    os.chdir(save_location)
 
-def captureImages():
-    gp(triggerCommand)
-    sleep(3)
-    gp(downloadCommand)
-    gp(clearCommand)
+def capture_image(album_location):
+    img_path = album_location + datetime.now().strftime("%Y.%m.%d %H:%M:%S") + ".JPG"
+    captureAndDownloadCommand[2] = img_path
+    gp(captureAndDownloadCommand)
+    return img_path
 
-def renameFiles(ID):
-    for filename in os.listdir("."):
-        if len(filename) < 13:
-            if filename.endswith(".JPG"):
-                os.rename(filename, (ID + "_" + shot_time + ".JPG"))
-                print("Renamed the JPG")
+# def captureImages():
+#     gp(triggerCommand)
+#     sleep(3)
+#     gp(downloadCommand)
+#     gp(clearCommand)
+#
+# def renameFiles(ID):
+#     shot_time = datetime.now().strftime("%Y.%m.%d %H:%M:%S")
+#     for filename in os.listdir("."):
+#         if len(filename) < 13:
+#             if filename.endswith(".JPG"):
+#                 os.rename(filename, (ID + "_" + shot_time + ".JPG"))
+#                 print("Renamed the JPG")
 
- 
-killgphoto2process()
-gp(clearCommand)
-createSaveFolder()
-captureImages()
-renameFiles(picID)
+
+# killgphoto2process()
+# gp(clearCommand)
+# createSaveFolder()
+# captureImages()
+# renameFiles(picID)
