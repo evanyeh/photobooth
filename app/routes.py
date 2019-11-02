42bdd673b6b5d39e92fe3e9ef7519425dd769b27
evanyeh <eyeh2809@yahoo.com>
2019-11-01 19:44:01 -0700

    Add image capture and return

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,12 @@ from flask import render_template, url_for, request, redirect, current_app
 from app import app
 import os
 import base64 # for image displaying
+import datetime # for timing out
+import time
+from . import camera
+
+session_name = "fall"
+album_location = "/home/evan/Pictures/photobooth/" + session_name + "/"
 
 @app.route('/')
 @app.route('/index')
@@ -16,6 +22,7 @@ def preview():
 
 @app.route('/capture')
 def capture_sequence():
+    camera.init(session_name)
     return render_template('capture.html')
 
 # @app.route('/background_process_test')
@@ -29,10 +36,21 @@ def capture_sequence():
 @app.route('/trigger_and_return_picture')
 def trigger_and_return_picture():
     # trigger capture
+    img_path = camera.capture_image(album_location)
+
+    # wait for image to come in
+    start_time = datetime.datetime.now()
+    while not os.path.exists(img_path):
+        # wait here
+        if (datetime.datetime.now() - start_time).total_seconds() > 6:
+            print("Timeout")
+            break
+
     # return image
-    with current_app.open_resource("dog.jpg") as img_file:
-        my_string = base64.b64encode(img_file.read())
-    return my_string
+    with open(img_path, 'rb') as img_file:
+        img_string = base64.b64encode(img_file.read())
+
+    return img_string
 
 @app.route('/distribute')
 def distribute():
