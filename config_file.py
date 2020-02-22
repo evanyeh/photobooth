4f51a29bc0993c6a0cd62383473139ccd695c318
evanyeh <eyeh2809@yahoo.com>
2020-02-21 23:03:24 -0800

    Change styling to winter picnic

--- config_file.py
+++ config_file.py
@@ -1,9 +1,7 @@
 # Image processing
-email_username = "username@gmail.com"
-email_password = "password"
 
 # Image capture
-session_name = "fallparty"
+session_name = "winterpicnictest"
 album_location = "/home/evan/Pictures/photobooth/" + session_name + "/"
 IMAGE_TIMEOUT = 6
 num_pictures = 1
@@ -11,15 +9,15 @@ countdown_time = 5
 
 # Styling
 styling = { \
-"font_file" : "https://fonts.googleapis.com/css?family=Gloria+Hallelujah&display=swap", \
-"font_name" : "Gloria Hallelujah", \
-"primary_bg_color" : "d98e71", \
-"secondary_bg_color" : "f5e4ae", \
-"primary_font_color" : "ffffff", \
-"secondary_font_color" : "836953"}
+"font_file" : "https://fonts.googleapis.com/css?family=Itim&display=swap", \
+"font_name" : "Itim", \
+"primary_bg_color" : "8cb9b6", \
+"secondary_bg_color" : "dad2bf", \
+"primary_font_color" : "000000", \
+"secondary_font_color" : "f45a24"}
 
 
 # Email
 email_from = "Suite 214 (:"
-email_subject = 'Suite 214 Fall is Life Gathering Photos'
-email_body = '<p>Thanks for hanging out with us! We hope you had a good time (:&nbsp;</p><p>  <br></p><p>  <a href="https://youtu.be/eupRD3QD6yM?t=92">Season\'s greetings</a>,</p><p>'
+email_subject = 'Suite 214 Picnic Photos'
+email_body = '<p>Thanks for hanging out with us! We hope you had a good time (:&nbsp;</p><p>  <br></p><p>  <a href="https://twitter.com/MichaelaOkla/status/1195771517868498944">hav a gud winter term</a>,</p><p>'
