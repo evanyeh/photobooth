41bec897b30a0a413ba41af88368837a60a665e4
Evan Yeh <eyeh2809@yahoo.com>
2022-11-19 17:22:28 -0800

    Fall 2022 styling update and some improvements

--- config_file.py
+++ config_file.py
@@ -1,8 +1,8 @@
 ################################ Image capture #################################
 # for displaying in idle photobooth page
-event_name = "Happy Birthdays Bay Baes!!"
+event_name = "Humpty Dumpty Had a Great Fall"
 # for creating a subfolder under some larger photobooth directory, see album_location
-session_name = "2022-02-19 Feb Birthday"
+session_name = "2022-11-19 Humpty Dumpty Fall Party"
 # where all the photos are saved, should be auto-created if doesn't already exist
 album_location = "/home/evan/Pictures/photobooth/" + session_name + "/"
 # time in seconds to wait for an image to come back from frontend (non-neg any)
@@ -14,22 +14,22 @@ num_pictures = 3
 # but countdown only goes 100ms at a time
 trigger_pre_count = 1.3
 # countdown time in seconds before each picture (pos int)
-countdown_time = 5
+countdown_time = 4
 # time in seconds between each picture, to allow review (pos any)
 time_between_pictures = 3
 
 ################################ Google Photos #################################
-google_photos_album_name = "2022 bruary bay bae birthdays"
+google_photos_album_name = "2022 Humpty Dumpty Had a Great Fall"
 
 ################################# Web Styling ##################################
 # can browse https://fonts.google.com/ for ez fonts
 styling = { \
-"font_file" : "https://fonts.googleapis.com/css2?family=Neucha&display=swap", \
-"font_name" : "Neucha", \
-"primary_bg_color" : "#2D3047", \
-"secondary_bg_color" : "#ED217C", \
-"primary_font_color" : "#FFFD82", \
-"secondary_font_color" : "#1B998B"}
+"font_file" : "https://fonts.googleapis.com/css?family=Gloria+Hallelujah&display=swap", \
+"font_name" : "Gloria Hallelujah", \
+"primary_bg_color" : "#d98e71", \
+"secondary_bg_color" : "#f5e4ae", \
+"primary_font_color" : "#ffffff", \
+"secondary_font_color" : "#836953"}
 
 ############################# Image distirbution ###############################
 # email info to send out pictures right after theyre taken
@@ -39,12 +39,12 @@ email_username = "username@website.com"
 email_password = "password"
 # pseudonym that replaces email_username 
 email_from = "evan's photobooth"
-email_subject = '2022 Avery Bay Birthday Photos'
+email_subject = '2022 Humpty Dumpty Had a Great Fall'
 # write body in HTML
-email_body = '<p>Your photos are attached! View the complete album here: <a href="https://photos.app.goo.gl/inBB8pB21WQKUzmz8">https://photos.app.goo.gl/inBB8pB21WQKUzmz8</a></p><p>Happy birthday '
+email_body = '<p>Your photos are attached! View the complete album here: <a href="https://photos.app.goo.gl/SPb49krwt7hEtfd89">https://photos.app.goo.gl/SPb49krwt7hEtfd89</a></p><p>Your friends, </p><p>'
 # routes.py/send_mail() will shuffle the below names and append it to the email_body
 # so no member gets preferential ordering :)
-signature_members = ['Betty', 'Miriam', 'Rachel', 'Tanvi']
+signature_members = ['Evan', 'Daniel', 'Asim']
 
 # Development
 DEBUG_MODE = False # primarily for bypassing camera intiation
